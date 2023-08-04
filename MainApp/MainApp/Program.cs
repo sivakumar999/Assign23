e54fbf7405abc98b585e734c431c40c10cdde932
sivakumar999 <sivakumar59498@gmail.com>
2023-08-04 15:48:05 +0530

    Done

---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your email address: ");
+            string email = Console.ReadLine();
+
+            if (Utility.IsValidEmail(email))
+            {
+                Console.WriteLine("Valid email address.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ MySharedLibrary/MySharedLibrary/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MySharedLibrary
+{
+    public class Utility
+    {
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            // Use regular expression pattern to validate email format
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+    }
+
+}
